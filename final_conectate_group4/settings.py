60587d6058ec59263b06139477c3bae661baf32b
Cristian <corpari25@gmail.com>
2018-03-22 19:14:28 -0500

    first commit with app ready for deplyment

--- final_conectate_group4/settings.py
+++ final_conectate_group4/settings.py
@@ -116,6 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'final_conectate_group4/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
