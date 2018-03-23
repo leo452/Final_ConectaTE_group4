a7fd6eb241b881ac42f4940d6c2a2bad64564696
Cristian <corpari25@gmail.com>
2018-03-22 19:29:54 -0500

    first commit with app ready for deplyment

--- final_conectate_group4/settings.py
+++ final_conectate_group4/settings.py
@@ -118,8 +118,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'final_conectate_group4/static'),
-]
--- final_conectate_group4/wsgi.py
+++ final_conectate_group4/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "final_conectate_group4.settings")
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(application)
