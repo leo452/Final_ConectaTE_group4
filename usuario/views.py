e2b562b4af53cfc05e6394b54f684f462b89811d
dfSabogal10 <david.sabogal1@gmail.com>
2018-03-26 00:11:49 -0500

    login con correo

--- usuario/views.py
+++ usuario/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth import authenticate, login, get_user_model, logout, update_session_auth_hash
 from django.core import serializers
 from django.http import Http404
 
@@ -14,16 +14,21 @@ def index(request):
 @csrf_exempt
 def login_rest(request):
     if request.method == 'POST':
-        username = request.POST.get("username")
+        email = request.POST.get("email")
         password = request.POST.get("password")
-        user = authenticate(username=username, password=password)
-        print(user)
-    if user is not None:
-        login(request,user)
-        return HttpResponse(serializers.serialize("json", [user]))
-    else:
-        mensaje= 'Nombre de usuario o clave no valido'
-        raise Http404(mensaje)
+        UserModel=get_user_model()
+        try:
+            usuario=UserModel.objects.get(email=email)
+        except UserModel.DoesNotExist:
+            mensaje = 'Nombre de usuario o clave no valido'
+            raise Http404(mensaje)
+        else:
+            if usuario.check_password(password):
+                login(request, usuario)
+                return HttpResponse(serializers.serialize("json", [usuario]))
+            else:
+                mensaje = 'Nombre de usuario o clave no valido'
+                raise Http404(mensaje)
 
 
 def login_view(request):
