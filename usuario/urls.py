39538c9a7ec2d8272359cc8e2084b39e1f89175d
Nicolas Robles <nr.robles@uniandes.edu.co>
2018-03-27 03:01:56 -0500

    vista y template para edicion de perfiles de los usuarios

--- usuario/urls.py
+++ usuario/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.login_rest, name='login'),
-    url(r'^loginview$', views.login_view, name='loginview')
+    url(r'^loginview$', views.login_view, name='loginview'),
+    url(r'^editarperfiles/', views.admin_edicion_perfiles, name="editarPerfiles"),
+    url(r'^cambiagrupo/', views.admin_cambia_grupo)
+    #url(r'^cambiaGrupo/(\d+)/(\d+)', views.admin_cambia_grupo)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- usuario/views.py
+++ usuario/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, get_user_model, logout, update_session_auth_hash
 from django.core import serializers
 from django.http import Http404
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import Group
+from usuario.models import Usuario
+from herramienta.models import HerramientaEdicion
+from django.core.paginator import Paginator
+from django.template import RequestContext
 
 
 # Create your views here.
@@ -33,3 +39,83 @@ def login_rest(request):
 
 def login_view(request):
     return render(request, 'login.html')
+
+
+def in_admin_group(user):
+    return user.is_authenticated() and 'Admin' in user.groups.iterator()
+
+
+def paginator(request, query, paginas):
+    result_list = Paginator(query, paginas)
+    try:
+        page = int(request.GET.get('page'))
+    except:
+        page = 1
+
+    if page <= 0:
+        page = 1
+
+    if page > result_list.num_pages:
+        page = result_list.num_pages
+
+    if result_list.num_pages >= page:
+        pagina = result_list.page(page)
+        context= {
+            'queryset': pagina.object_list,
+            'page': page,
+            'pages': result_list.num_pages,
+            'has_next': pagina.has_next(),
+            'has_prev': pagina.has_previous(),
+            'next_page': page+1,
+            'prev_page': page-1,
+            'firstPage': 1,
+        }
+        return context
+
+
+#@user_passes_test(in_admin_group, login_url='usuario/loginview')
+def admin_edicion_perfiles(request):
+    grupos = Group.objects.all()
+    user_list = Usuario.objects.all()
+    pag = paginator(request, user_list, 10)
+    cxt = {
+        'user_pag': pag['queryset'],
+        'user_list': user_list,
+        'paginator': pag,
+        'grupos': grupos
+    }
+    return render(request, 'editarperfiles.html', cxt)
+
+@csrf_exempt
+def admin_cambia_grupo(request):
+    if request.method == 'POST':
+        id = int(request.POST.get("id"))
+        uGrupo = int(request.POST.get("grupo"))
+        user = get_object_or_404(Usuario, id=id)
+        group = get_object_or_404(Group, id=uGrupo)
+
+        user_group = Usuario.groups.through.objects.get(user=user)
+        user_group.group = group
+        user_group.save()
+
+        return HttpResponse("actualizado",content_type="text/plain")
+
+
+def usuarios(request):
+    usuarios = Usuario.objects.all()
+    return HttpResponse(serializers.serialize("json", usuarios))
+
+def usuarioHeramientas(request, uId):
+    usuherramienta = HerramientaEdicion.objects.all().filter(usuarioHerramienta = uId)
+    return HttpResponse(serializers.serialize("json", usuherramienta))
+
+
+def admin_ver_usuario_herramienta(request):
+    user_list = Usuario.objects.all()
+    pag = paginator(request, user_list, 10)
+    cxt = {
+        'user_pag': pag['queryset'],
+        'user_list': user_list,
+        'paginator': pag
+    }
+    return render(request, 'usuarioherramienta.html', cxt)
