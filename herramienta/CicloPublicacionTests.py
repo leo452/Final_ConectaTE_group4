adf27ddc912d066e982051bec6dc2da94cccfe75
Cristian Paez <corpari25@gmail.com>
2018-04-19 16:19:07 -0500

    (Green) Solicitar Revisión de pares Test

--- herramienta/CicloPublicacionTests.py
+++ herramienta/CicloPublicacionTests.py
@@ -35,13 +35,36 @@ class Test(TestCase):
         btn_login = self.browser.find_element_by_id('btn_login')
         btn_login.click()
 
-        btn_login = self.browser.find_element_by_id('herramienta_4_nombre') #Herramienta en estado de revision propia
-        btn_login.click()
+        btn_tool = self.browser.find_element_by_id('herramienta_4_nombre') #Herramienta en estado de revision propia
+        btn_tool.click()
 
-        btn_edicion = self.browser.find_element_by_id('comenzar_edicion') #Herramienta en estado de revision propia
+        btn_edicion = self.browser.find_element_by_id('comenzar_edicion')
         btn_edicion.click()
 
-        
+    def test_solicitarRevision(self):
+        self.browser.get('http://localhost:8080/herramientas')
+        #self.browser.get('https://final-conectate-group4.herokuapp.com/herramientas')
+
+        self.browser.get('http://localhost:8080/herramientas')
+        #self.browser.get('https://final-conectate-group4.herokuapp.com/herramientas')
+
+        self.browser.implicitly_wait(10000)
+
+        link = self.browser.find_element_by_id('login')
+        link.click()
+        self.browser.implicitly_wait(3000)
+        input_email = self.browser.find_element_by_id('email')
+        input_email.send_keys(usr_miembroGti2)
+        input_pass = self.browser.find_element_by_id('password')
+        input_pass.send_keys(pwd_miembroGti2)
+        btn_login = self.browser.find_element_by_id('btn_login')
+        btn_login.click()
+
+        btn_tool = self.browser.find_element_by_id('herramienta_10_nombre') #Herramienta en estado de revision propia
+        btn_tool.click()
+
+        btn_revision = self.browser.find_element_by_id('solicitar_revision_pares')
+        btn_revision.click()
 
     def test_MarcarListoRevision(self):
         self.browser.get('http://localhost:8080/herramientas')
@@ -59,8 +82,8 @@ class Test(TestCase):
         btn_login = self.browser.find_element_by_id('btn_login')
         btn_login.click()
 
-        btn_login = self.browser.find_element_by_id('herramienta_4_nombre') #Herramienta en estado de revision
-        btn_login.click()
+        btn_tool = self.browser.find_element_by_id('herramienta_4_nombre') #Herramienta en estado de revision
+        btn_tool.click()
 
         btn_postulacion = self.browser.find_element_by_id('postular_a_publicacion')
         btn_postulacion.click()
