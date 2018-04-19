96107fabff0f1a8bcf6cce092797fe13adb0f643
Cristian Paez <corpari25@gmail.com>
2018-04-19 16:04:05 -0500

    (Green) Iniciar edición de Herramienta

--- herramienta/CicloPublicacionTests.py
+++ herramienta/CicloPublicacionTests.py
@@ -6,6 +6,9 @@ from selenium import webdriver
 usr_miembroGti = "miembro@uniandes.edu.co"
 pwd_miembroGti = "miembro123456"
 
+usr_miembroGti2 = "miembro2@uniandes.edu.co"
+pwd_miembroGti2 = "miembro123456"
+
 class Test(TestCase):
 
     def setUp(self):
@@ -15,6 +18,30 @@ class Test(TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def test_comenzarEdicion(self):
+        self.browser.get('http://localhost:8080/herramientas')
+        #self.browser.get('https://final-conectate-group4.herokuapp.com/herramientas')
+
+        self.browser.implicitly_wait(10000)
+
+        link = self.browser.find_element_by_id('login')
+        link.click()
+        self.browser.implicitly_wait(3000)
+        input_email = self.browser.find_element_by_id('email')
+        input_email.send_keys(usr_miembroGti2)
+        input_pass = self.browser.find_element_by_id('password')
+        input_pass.send_keys(pwd_miembroGti2)
+        btn_login = self.browser.find_element_by_id('btn_login')
+        btn_login.click()
+
+        btn_login = self.browser.find_element_by_id('herramienta_4_nombre') #Herramienta en estado de revision propia
+        btn_login.click()
+
+        btn_edicion = self.browser.find_element_by_id('comenzar_edicion') #Herramienta en estado de revision propia
+        btn_edicion.click()
+
+        
 
     def test_MarcarListoRevision(self):
         self.browser.get('http://localhost:8080/herramientas')
