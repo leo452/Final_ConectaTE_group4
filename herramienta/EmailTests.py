81561c5a20a8d947701dc18417ccae89e854b171
Cristian <corpari25@gmail.com>
2018-05-13 21:01:13 -0500

    (Red) PC40 Envio de correo a los usuarios con el rol de administrado cuando una herramienta es postalada para publicación

--- herramienta/EmailTests.py
+++ herramienta/EmailTests.py
@@ -43,3 +43,29 @@ class Test(TestCase):
 
         btn_revision = self.browser.find_element_by_id('solicitar_revision_pares')
         btn_revision.click()
+
+    def test_send_public_tool_email(self):
+        self.browser.get('http://localhost:8080/herramientas')
+        # self.browser.get('https://final-conectate-group4.herokuapp.com/herramientas')
+
+        self.browser.implicitly_wait(10000)
+
+        link = self.browser.find_element_by_id('login')
+        link.click()
+        self.browser.implicitly_wait(3000)
+        input_email = self.browser.find_element_by_id('email')
+        input_email.send_keys(usr_miembroGti)
+        input_pass = self.browser.find_element_by_id('password')
+        input_pass.send_keys(pwd_miembroGti)
+        btn_login = self.browser.find_element_by_id('btn_login')
+        btn_login.click()
+
+        btn_tool = self.browser.find_element_by_id('herramienta_15_nombre')  # Herramienta en estado de revision
+        btn_tool.click()
+
+        btn_postulacion = self.browser.find_element_by_id('postular_a_publicacion')
+        btn_postulacion.click()
+
+        self.browser.implicitly_wait(3000)
+
+        self.browser.find_element_by_id("postulacion_enviada")
